fc7f0a32a5fda73c702f7ce2019c6c38f2a97d7d
Lactor <machado007@gmail.com>
2016-12-01 16:20:18 -0800

    Added Description of states as an initializer

--- Evolution_States.py
+++ Evolution_States.py
@@ -14,11 +14,12 @@ args = {'Nsteps': 10**8,
         'SVD_Cutoff': 1e-7,
         'EIG_COUNTER': 25,
         'Obs_Val': 2,
-        '-o': './'}
+        '-o': './',
+        'stateDesc' : "X"}
 
 for i in range(2, len(sys.argv), 2):
     if sys.argv[i] in args.keys():
-        if sys.argv[i] == '-o':
+        if sys.argv[i] == '-o' or sys.argv[i] == 'stateDesc':
             args[ sys.argv[i]] = sys.argv[i+1]
         else:
             args[ sys.argv[i]] = float(sys.argv[i+1])
@@ -32,18 +33,38 @@ SVD_Cutoff = args['SVD_Cutoff']
 output_folder = args['-o']
 Obs_Val = int(args['Obs_Val'])
 
-fil = preComputation_Fil + "_State%d"%( StateNum) + ('_Log%d_Nsteps%d_MAX_COUNTER_%d_ObsVal_%d'%( logEvo, Nsteps, args['MAX_COUNTER'], Obs_Val) )
+L = int(np.log2(len(States[0]))+0.001)
+state = States[0] * 0
+if not (args['stateDesc'] == "X"):
+    StateNum = 0
+    for i in range(L):
+        if args['stateDesc'][i] == 'U':
+            StateNum += 2**i
+            print StateNum
+
+else:
+    args['stateDesc']= ""
+    for i in range():
+        if StateNum % 2**i == 0:
+            args['stateDesc'] += "U"
+        else:
+            args['stateDesc'] += "D"
+
+
+
+fil = preComputation_Fil + "_State%s"%(args['stateDesc']) + ('_Log%d_Nsteps%d_MAX_COUNTER_%d_ObsVal_%d'%( logEvo, Nsteps, args['MAX_COUNTER'], Obs_Val) )
 
 (temp, fil) = os.path.split(fil)
-
-print "Computing Evolution for state: ", States[StateNum]
+state[StateNum] = 1
+print "Computing Evolution for state: ", state
+print args['stateDesc']
 print "Saving it as:"
 print output_folder + fil
 
 
 
 Compute_Evolution(Nsteps,
-                  States[StateNum],
+                  state,
                   result_fil = fil,
                   preComputation_fil = preComputation_Fil,
                   logEvo = logEvo,
